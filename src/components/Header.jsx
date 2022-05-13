5716511ee5a82fcf49d509b5195562678c68353b
EthanChenYen-Peng <ypcethan@gmail.com>
2022-05-13 11:26:57 +1000

    adding font family

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,17 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
+import { sizes } from "../constants";
 
-export default function Header(){
-  return(
+export default function Header() {
+  return (
     <Container>
       <p>Header</p>
     </Container>
-    )
+  );
 }
 
-
 const Container = styled.div`
+  @media (min-width: ${sizes.sm}) {
+    background-color: red;
+  }
 `
--- src/globalStyle.js
+++ src/globalStyle.js
@@ -1,7 +1,8 @@
-import { createGlobalStyle } from 'styled-components'
-
+import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Barlow:wght@400;600&family=Fraunces:opsz,wght@9..144,700;9..144,900&display=swap');
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
@@ -14,13 +15,18 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
   }
 
+
+  body {
+    font-family: 'Barlow', sans-serif;
+  }
+
   :root {
     --soft-red: hsl(7, 99%, 70%);
     --yellow: hsl(51, 100%, 49%);
     --dark-desaturated-cyan: hsl(167, 40%, 24%);
     --dark-blue: hsl(198, 62%, 26%);
     --dark-moderate-cyan: hsl(168, 34%, 41%);
-  }
-`
+@import url('https://fonts.googleapis.com/css2?family=Barlow:wght@400;600&family=Fraunces:opsz,wght@9..144,700;9..144,900&display=swap');
+`;
 
-export default GlobalStyle
+export default GlobalStyle;
